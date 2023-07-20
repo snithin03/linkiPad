06569951ac79249cab4e0ff1c39d2f35b6711c1e
snithin03 <sainithin31200@gmail.com>
2023-07-20 14:46:56 -0400

    Challenge 2

--- main.py
+++ main.py
@@ -121,63 +121,12 @@ def json_transformer(input_json):
     return [result] if result else None
 
 
-# given JSON input string
-json_string = """
-{
-  "number_1": {
-    "N": "1.50"
-  },
-  "string_1": {
-    "S": "784498 "
-  },
-  "string_2": {
-    "S": "2014-07-16T20:55:46Z"
-  },
-  "map_1": {
-    "M": {
-      "bool_1": {
-        "BOOL": "truthy"
-      },
-      "null_1": {
-        "NULL ": "true"
-      },
-      "list_1": {
-        "L": [
-          {
-            "S": ""
-          },
-          {
-            "N": "011"
-          },
-          {
-            "N": "5215s"
-          },
-          {
-            "BOOL": "f"
-          },
-          {
-            "NULL": "0"
-          }
-        ]
-      }
-    }
-  },
-  "list_2": {
-    "L": "noop"
-  },
-  "list_3": {
-    "L": [
-      "noop"
-    ]
-  },
-  "": {
-    "S": "noop"
-  }
-}
-"""
+#Specify the file path
+file_path = "input.json"
 
-# Converting the JSON string to a dictionary
-json_dict = json.loads(json_string)
+#Loading the contents of json file
+with open(file_path, "r") as file:
+    json_dict = json.load(file)
 
 # Transforming the JSON input
 transformed_json = json_transformer(json_dict)
